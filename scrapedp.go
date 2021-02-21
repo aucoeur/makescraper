27370ef1ec85637dcd324f2f06ef5329a8b21bf1
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-02-21 14:08:26 -0800

    splitting up the selectors

--- scrapedp.go
+++ scrapedp.go
@@ -17,57 +17,92 @@ import (
 // 		chromedp.Navigate(`https://leetcode.com/problemset/all/?search=two`),
 
 // 		// Wait until the bottom of the page loads (where you can find the show all & pagination)
-// 		chromedp.WaitVisible(`.reactable-pagination`),
+// 		chromedp.WaitVisible(`.reactable-pagination`, chromedp.ByQuery),
+
+// 		// Scrolls to footer
+// 		chromedp.ScrollIntoView(`#footer-root`, chromedp.ByID),
 
 // 		// Selector `td[label='Title'] > div > a:only-child` gets all the free/public problems
-// 		chromedp.ScrollIntoView(`td[label='Title'] > div > a:only-child`),
-
-// 		// chromedp.Nodes(`td[label='Title'] > div > a:only-child`, &nodes),
-// 		chromedp.Nodes(`td[label='Title'] > div > a:only-child`, &nodes, chromedp.ByQuery),
+// 		chromedp.Nodes(`td[label='Title'] > div > a:only-child`, &nodes, chromedp.ByQueryAll),
 // 	}
 // }
 
+// Problem struct stores basic data of each problem
+type Problem struct {
+	ID         string `json:"id"`
+	Title      string `json:"title"`
+	URL        string `json:"url"`
+	Difficulty string `json:"difficulty"`
+}
+
 func main() {
-	// create chrome instance
+	// set chromedp options
 	opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		chromedp.DisableGPU,
 		// Set the headless flag to false to display the browser window
-		chromedp.Flag("headless", false),
+		chromedp.Flag("headless", true),
 	)
 
-	// Start new chrome instance / context
+	// "ExecAllocator is an Allocator which starts new browser processes on the host machine"
 	fmt.Println("Creating new instance..")
 	ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 
+	// Start new chrome instance / context
 	ctx, cancel = chromedp.NewContext(ctx)
-	// ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
 	defer cancel()
 
-	// Create array of cdproto type Node
-	var nodes []*cdp.Node
+	// ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+
+	// Create arrays of cdproto type Node
+	// var nodes []*cdp.Node
+	var rows []*cdp.Node
+	// var titles []map[string]string
 
 	// Run action/task list (yoinkcode)
-	// err := chromedp.Run(context, yoinkCode(nodes))
+	// if err := chromedp.Run(ctx, yoinkCode(nodes)); err != nil {
+	// 	fmt.Printf("something ducked up: %s", err)
+	// }
+
 	if err := chromedp.Run(ctx,
 		// chromedp.Navigate(`https://leetcode.com/problemset/all/`),
-		chromedp.Navigate(`https://leetcode.com/problemset/all/?search=two`),
+
+		// Starting smol..
+		chromedp.Navigate(`https://leetcode.com/problemset/all/?search=power%20of&difficulty=Easy`),
 
 		// Wait until the bottom of the page loads (where you can find the show all & pagination)
 		chromedp.WaitVisible(`.reactable-pagination`, chromedp.ByQuery),
 
-		// Selector `td[label='Title'] > div > a:only-child` gets all the free/public problems
+		// Scrolls to footer
 		chromedp.ScrollIntoView(`#footer-root`, chromedp.ByID),
 
-		// chromedp.Nodes(`td[label='Title'] > div > a:only-child`, &nodes),
-		// chromedp.Nodes(`a`, &nodes),
-		chromedp.Nodes(`td[label='Title'] > div > a:only-child`, &nodes, chromedp.ByQueryAll),
+		// Start with table rows
+		chromedp.Nodes(`.reactable-data > tr`, &rows, chromedp.ByQueryAll),
+
+		// prints out: 2021/02/21 13:32:44 map[label:Title value:Consecutive Characters]
+		// chromedp.AttributesAll(`td[label='Title']`, &titles, chromedp.ByQueryAll),
+
+		// Selector `td[label='Title'] > div > a:only-child` gets all the free/public problem links
+		// chromedp.Nodes(`td[label='Title'] > div > a:only-child`, &nodes, chromedp.ByQueryAll),
 	); err != nil {
 		fmt.Printf("something ducked up: %s", err)
 	}
+	log.Println("Found rows", len(rows))
 
-	// fmt.Println(nodes[0].AttributeValue("href"))
-	for i := 0; i < len(nodes); i++ {
-		log.Println(nodes[i].AttributeValue("href"))
+	// var ProblemSets []*Problem
+
+	// loops through td nodes
+	const childSelector = `.reactable-data > tr:nth-child(%d) > td:nth-child(2)`
+	var col string
+	for i := 0; i < len(rows); i++ {
+		// log.Println(nodes[i].AttributeValue("href"))
+		if err := chromedp.Run(ctx,
+			chromedp.Text(fmt.Sprintf(childSelector, i+1), &col),
+		); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("row %d: value = %s\n", i, col)
+		// log.Println(titles[i])
 	}
 }
